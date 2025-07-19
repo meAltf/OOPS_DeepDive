74f8f0cdb09b1fc0bad8fa20e325648c9f7d7629
meAltf <alataf.dce.cs1@gmail.com>
2025-07-19 18:50:26 +0530

    Practice code | Inheritance + method overriding + super

---
+++ src/Day_2/PracticeCode.java
@@ -0,0 +1,42 @@
+package Day_2;
+
+import java.time.Period;
+
+class Person {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void displayInfo() {
+        System.out.println("name: " + name + " Age: " + age);
+    }
+}
+
+class Employee extends Person {
+    double salary;
+    String designation;
+
+    Employee(String name, int age, double salary, String designation) {
+        super(name, age); // calling person constructor
+        this.salary = salary;
+        this.designation = designation;
+    }
+
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("salary: " + salary + " designation: " + designation);
+    }
+
+}
+
+public class PracticeCode {
+
+    public static void main(String[] args) {
+        Employee employee = new Employee("Robert", 23, 350000.00, "Data Scientist");
+        employee.displayInfo();
+    }
+}
