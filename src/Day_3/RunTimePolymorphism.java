5c2fda0411d45e351f98c7af891bb36d253232df
meAltf <alataf.dce.cs1@gmail.com>
2025-07-21 22:08:09 +0530

    Runtime polymorphism | code example

---
+++ src/Day_3/RunTimePolymorphism.java
@@ -0,0 +1,34 @@
+package Day_3;
+
+class Animal {
+    void sound() {
+        System.out.println("Animal makes sound!");
+    }
+}
+
+class Dog extends Animal {
+
+    @Override
+    void sound() {
+        System.out.println("Dog barks!");
+    }
+}
+
+class Cat extends Animal {
+
+    @Override
+    void sound() {
+        System.out.println("Cat meows!");
+    }
+}
+
+public class RunTimePolymorphism {
+
+    public static void main(String[] args) {
+        Animal animal1 = new Dog(); // Upcasting
+        Animal animal2 = new Cat();
+
+        animal1.sound(); // Dog
+        animal2.sound(); // Cat
+    }
+}
